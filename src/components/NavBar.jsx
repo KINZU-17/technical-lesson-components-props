b079225fc0fe0f893f4a94aa9193076352c211c9
James Nzuki <nzukijames200@gmail.com>
2026-05-01 18:04:49 +0300

    final display

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav id="navbar">
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
+  );
+}
+
+export default NavBar;
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,6 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "James Bond",
+  city: "London",
   bio: "I made this!",
   color: "firebrick",
   links: {
